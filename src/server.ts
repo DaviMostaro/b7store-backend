3671ea178eeba73b9dc0b47c5f39290a3a07600a
Davi_Mostaro <davicafezinho15@gmail.com>
2025-08-25 13:56:03 -0300

    feat: creating the first route

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import express, { type NextFunction, type Request, type Response } from 'express';
+import cors from 'cors';
+import { routes } from './routes/main.js';
+
+const server = express();
+server.use(cors());
+server.use(express.static('public'));
+server.use(express.json());
+
+server.use(routes);
+
+server.use((err: any, req: Request, res: Response, next: NextFunction) => {
+    console.log(err);
+    res.status(500).json({ error: err.message });
+});
+
+const port = process.env.PORT || 4000;
+server.listen(port, () => {
+    console.log(`B7Store running on port ${port}`);
+});
