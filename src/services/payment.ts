8881fc9e1b0ffec397a222b69792b4b8cec8a86f
Davi_Mostaro <davicafezinho15@gmail.com>
2025-09-02 13:13:44 -0300

    feat: creating endpoint to get order by session

--- src/services/payment.ts
+++ src/services/payment.ts
@@ -1,4 +1,4 @@
-import { createStripeCheckoutSession } from "../libs/stripe";
+import { createStripeCheckoutSession, getStripeCheckoutSession } from "../libs/stripe";
 import { CartItem } from "../types/cart-item"
 
 type CreatePaymentLinkParams = {
@@ -13,6 +13,19 @@ export const createPaymentLink = async ({ cart, shippingCost, orderId }: CreateP
         if(!session.url) return null;
         return session.url;
     } catch {
+        return null;
+    }
+}
+
+export const getOrderIdFromSession = async (sessionId: string) => {
+    try {
+        const session = await getStripeCheckoutSession(sessionId);
+        const orderId = session.metadata?.orderId;
+
+        if(!orderId) return null;
+
+        return parseInt(orderId);
+    }catch {
         return null;
     }
 }
